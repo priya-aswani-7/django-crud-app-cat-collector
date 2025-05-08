72ca181b86ad549896bc40de816ae653318f283a
priya-aswani-7 <priya.aswani28139@gmail.com>
2025-05-08 21:29:49 +0400

    Creating cats template

--- main_app/urls.py
+++ main_app/urls.py
@@ -2,10 +2,12 @@ from django.urls import path
 from . import views # Import views to connect routes to view functions
 
 urlpatterns = [
-    path('', views.home, name='home'),
-    path('about', views.about, name='about'),
-    path('conclusion', views.conclusion, name='conclusion'),
+  path('', views.home, name='home'),
+  path('about/', views.about, name='about'),
+  # route for cats index
+  path('cats/', views.cat_index, name='cat-index'),
 ]
+
 
 # register a path called conclusion, handled by views.conclusion
 # define the conclusion function in views.py and it must return 
--- main_app/views.py
+++ main_app/views.py
@@ -5,6 +5,24 @@ from django.shortcuts import render
 # Import HttpResponse to send text-based responses
 from django.http import HttpResponse
 
+# views.py
+
+class Cat:
+    def __init__(self, name, breed, description, age):
+        self.name = name
+        self.breed = breed
+        self.description = description
+        self.age = age
+
+# Create a list of Cat instances
+cats = [
+    Cat('Lolo', 'tabby', 'Kinda rude.', 3),
+    Cat('Sachi', 'tortoiseshell', 'Looks like a turtle.', 0),
+    Cat('Fancy', 'bombay', 'Happy fluff ball.', 4),
+    Cat('Bonk', 'selkirk rex', 'Meows loudly.', 6)
+]
+
+
 # Define the home view function
 def home(request):
     # Send a simple HTML response
@@ -12,6 +30,13 @@ def home(request):
 
 def about(request):
     return render(request, 'about.html')
+
+# views.py
+
+def cat_index(request):
+    # Render the cats/index.html template with the cats data
+    return render(request, 'cats/index.html', {'cats': cats})
+
 
 def conclusion(request):
     return render(request, 'conclusion.html')
