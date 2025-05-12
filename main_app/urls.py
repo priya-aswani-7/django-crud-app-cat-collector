45d51b7ac54c56c96295a73f49b7ca1bc94faf49
priya-aswani-7 <priya.aswani28139@gmail.com>
2025-05-12 21:49:27 +0400

    Allow viewing cat details

--- main_app/urls.py
+++ main_app/urls.py
@@ -2,10 +2,10 @@ from django.urls import path
 from . import views # Import views to connect routes to view functions
 
 urlpatterns = [
-  path('', views.home, name='home'),
-  path('about/', views.about, name='about'),
-  # route for cats index
-  path('cats/', views.cat_index, name='cat-index'),
+    path('', views.home, name='home'),
+    path('about/', views.about, name='about'),
+    path('cats/', views.cat_index, name='cat-index'),
+    path('cats/<int:cat_id>/', views.cat_detail, name='cat-detail'),
 ]
 
 
--- main_app/views.py
+++ main_app/views.py
@@ -1,24 +1,8 @@
 # main_app/views.py
 
 from django.shortcuts import render
-
-# views.py
-
-class Cat:
-    def __init__(self, name, breed, description, age):
-        self.name = name
-        self.breed = breed
-        self.description = description
-        self.age = age
-
-# Create a list of Cat instances
-cats = [
-    Cat('Lolo', 'tabby', 'Kinda rude.', 3),
-    Cat('Sachi', 'tortoiseshell', 'Looks like a turtle.', 0),
-    Cat('Fancy', 'bombay', 'Happy fluff ball.', 4),
-    Cat('Bonk', 'selkirk rex', 'Meows loudly.', 6)
-]
-
+from django.shortcuts import render
+from .models import Cat
 
 # Define the home view function
 def home(request):
@@ -29,17 +13,14 @@ def about(request):
     return render(request, 'about.html')
 
 # views.py
-
 def cat_index(request):
-    # Render the cats/index.html template with the cats data
+    cats = Cat.objects.all()  # look familiar?
     return render(request, 'cats/index.html', {'cats': cats})
 
+def cat_detail(request, cat_id):
+    cat = Cat.objects.get(id=cat_id)
+    return render(request, 'cats/detail.html', {'cat': cat})
 
 def conclusion(request):
     return render(request, 'conclusion.html')
 
-# create a template conclusion.html and render it inside of conclusion function in views.py
-# conclusion.html must extend base.html and display the following content in the body
-# h1: This is the conclusion
-# horizontal rule
-# unordered list of three items: thank you, goodbye, see you again
