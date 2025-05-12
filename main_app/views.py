35b7f9e27d3affcdf7777ed51ce3caced0c8c8a3
priya-aswani-7 <priya.aswani28139@gmail.com>
2025-05-12 23:03:10 +0400

    Add functionality to update and delete a cat

--- main_app/views.py
+++ main_app/views.py
@@ -1,12 +1,21 @@
 # main_app/views.py
 
 from django.shortcuts import render
-from django.views.generic.edit import CreateView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from .models import Cat
 
 class CatCreate(CreateView):
     model = Cat
     fields = '__all__'
+
+class CatUpdate(UpdateView):
+    model = Cat
+    # Let's disallow the renaming of a cat by excluding the name field!
+    fields = ['breed', 'description', 'age']
+
+class CatDelete(DeleteView):
+    model = Cat
+    success_url = '/cats/'
 
 # Define the home view function
 def home(request):
