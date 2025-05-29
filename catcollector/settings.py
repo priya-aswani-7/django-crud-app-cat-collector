b5cc8a313540f498b67ea2d5b8a1b8d19e506941
priya-aswani-7 <priya.aswani28139@gmail.com>
2025-05-29 23:49:22 +0400

    Add redirect url post login

--- catcollector/settings.py
+++ catcollector/settings.py
@@ -120,3 +120,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Add this variable to specify where successful logins should redirect to
+LOGIN_REDIRECT_URL = 'cat-index'
